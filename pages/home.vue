ca88a87a607091ed28f6e00a00d8ac37a1974af2
Siripongapr <siripong.api43@gmail.com>
2023-05-14 05:02:39 +0700

    Create nuxt project , add login page

---
+++ pages/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-card width="300" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">
+          Login
+        </h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+          <v-text-field type="password" label="Password" prepend-icon="mdi-lock" />
+        </v-form>
+      </v-card-text>
+      <v-divider />
+      <div style="margin-left: 10px;">
+        <v-btn color="success" class="mr-2">
+          Login
+        </v-btn>
+        <v-btn color="info">
+          Register
+        </v-btn>
+      </div>
+    </v-card>
+  </v-app>
+</template>
