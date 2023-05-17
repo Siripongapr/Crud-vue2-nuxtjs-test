be0f78233b2db69dcae782f351c08679c36add3b
Siripongapr <siripong.api43@gmail.com>
2023-05-17 12:43:07 +0700

    add dialog Edit

--- pages/tableSearch.vue
+++ pages/tableSearch.vue
@@ -1,6 +1,7 @@
 <template>
   <v-data-table
     v-model="selected"
+    light
     :headers="headers"
     :items="info"
     :items-per-page="5"
@@ -15,12 +16,37 @@
     <template #top>
       <v-toolbar flat color="#0D47A1">
         <v-container>
-          <v-toolbar-title>
-            <div>
-              Manage
-              <b>Employees</b>
-            </div>
-          </v-toolbar-title>
+          <v-row>
+            <v-col
+              cols="5"
+            >
+              <v-toolbar-title>
+                <div
+                  style="color:white;"
+                >
+                  Manage
+                  <b>Employees</b>
+                </div>
+              </v-toolbar-title>
+            </v-col>
+            <v-col
+              cols="7"
+              align="end"
+            >
+              <v-btn
+                color="green"
+                @click="openDialogAdd"
+              >
+                ADD NEW EMPLOYEE
+              </v-btn>
+              <v-btn
+                color="red"
+                @click="openDialogSelectedDelete"
+              >
+                DELETE
+              </v-btn>
+            </v-col>
+          </v-row>
         </v-container> <!--Header text -->
 
         <!--Dialog ADD New Employee-->
@@ -28,15 +54,6 @@
           v-model="dialogAdd"
           max-width="500px"
         >
-          <template #activator="{ on , attrs }">
-            <v-btn
-              color="green"
-              v-bind="attrs"
-              v-on="on"
-            >
-              Add New Employee
-            </v-btn>  <!--Add button-->
-          </template>
           <v-card>
             <v-card-title>Add New Employee</v-card-title>
             <v-card-text>
@@ -111,52 +128,68 @@
           v-model="dialogSelectedDelete"
           max-width="500px"
         >
-          <template #activator="{ on , attrs}">
-            <v-btn
-              v-bind="attrs"
-              color="red"
-              class="mx-2"
-              v-on="on"
-            >
-              delete <!--Delete Button -->
-            </v-btn>
-            <v-spacer />
-          </template>
-          <v-card-title>
-            <b>Delete Employees</b>
-          </v-card-title>
-          <v-card-text>
-            <div style="font-size: 15px;">
-              Confirm to delete all column
-            </div>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer />
-            <v-btn
-              color="red"
-              @click="dialogSelectedDelete = false"
-            >
-              Confirm
-            </v-btn>
-            <v-btn
-              color="blue"
-              @click="dialogSelectedDelete = false"
-            >
-              Cancel
-            </v-btn>
-          </v-card-actions>
+          <v-card>
+            <v-card-title>
+              <b>Delete Employees</b>
+            </v-card-title>
+            <v-card-text>
+              <div style="font-size: 15px;">
+                Confirm to delete all column
+              </div>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                color="red"
+                @click="dialogSelectedDelete = false"
+              >
+                Confirm
+              </v-btn>
+              <v-btn
+                color="blue"
+                @click="dialogSelectedDelete = false"
+              >
+                Cancel
+              </v-btn>
+            </v-card-actions>
+          </v-card>
         </v-dialog>
       </v-toolbar>
     </template>
 
-    <template slot="item.actions" slot-scope="{ item }">
-      <v-icon small class="mr-2" @click="editItem(item)">
-        mdi-pencil
-      </v-icon>
-      <v-icon small class="mr-2" @click="editItem(item)">
-        mdi-delete
-      </v-icon>
-    </template> //icon
+    <template slot="item.actions">
+      <v-btn
+        @click="openDialogEdit"
+      >
+        test dialog 1
+      </v-btn>
+      <!--Dialog Edit-->
+      <v-dialog
+        v-model="dialogEdit"
+        max-width="500px"
+        content-class="elevation-0"
+      >
+        <!-- content-class is for remove shadow box -->
+        <v-card>
+          <v-card-title>
+            Edit Employee
+          </v-card-title>
+          <v-divider />
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+              >
+                <v-text-field
+                  label="First Name"
+                  outlined
+                />
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-dialog>
+    </template> <!--Edit Delete dialog Icon-->
   </v-data-table>
 </template>
 
@@ -165,7 +198,9 @@ export default {
   data: () => ({
     selected: [],
     dialogAdd: false,
+    dialogEdit: false,
     dialogSelectedDelete: false,
+
     headers: [
       {
         text: 'No.',
@@ -179,123 +214,55 @@ export default {
       { text: 'Phone', value: 'phone' },
       { text: 'Actions', value: 'actions', sortable: false }
     ],
-    info: [],
-    editedIndex: -1,
-    editedItem: {
-      name: '',
-      email: '',
-      address: '',
-      phone: 0
-    },
-    defaultItem: {
-      name: '',
-      email: '',
-      address: '',
-      phone: 0
-    }
+    info: [
+      {
+        name: 'Thomas Hardy',
+        email: 'thomashardy@mail.com',
+        address: '89 Chiaroscuro Rd, Portland, USA',
+        phone: '(171) 555-2222'
+      },
+      {
+        name: 'Dominique Perrier',
+        email: 'dominiqueperrier@mail.com',
+        address: 'Obere Str. 57, Berlin, Germany',
+        phone: '(313) 555-5735'
+      },
+      {
+        name: 'Maria Anders',
+        email: 'dominiqueperrier@mail.com',
+        address: '25, rue Lauriston, Paris, France',
+        phone: '(503) 555-9931'
+      },
+      {
+        name: 'Fran Wilson',
+        email: 'franwilson@mail.com',
+        address: 'C/ Araquil, 67, Madrid, Spain',
+        phone: '(204) 619-5731'
+      },
+      {
+        name: 'Martin Blank',
+        email: 'martinblank@mail.com',
+        address: 'Via Monte Bianco 34, Turin, Italy',
+        phone: '(480) 631-2097'
+      },
+      {
+        name: 'Martin Blank',
+        email: 'martinblank@mail.com',
+        address: 'Via Monte Bianco 34, Turin, Italy',
+        phone: '(480) 631-2097'
+      }
+    ]
   }),
 
-  computed: {
-    formTitle () {
-      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
-    }
-  },
-
-  watch: {
-    dialog (val) {
-      val || this.close()
-    },
-    dialogDelete (val) {
-      val || this.closeDelete()
-    }
-  },
-
-  created () {
-    this.initialize()
-  },
-
   methods: {
-    initialize () {
-      this.info = [
-        {
-          name: 'Thomas Hardy',
-          email: 'thomashardy@mail.com',
-          address: '89 Chiaroscuro Rd, Portland, USA',
-          phone: '(171) 555-2222'
-        },
-        {
-          name: 'Dominique Perrier',
-          email: 'dominiqueperrier@mail.com',
-          address: 'Obere Str. 57, Berlin, Germany',
-          phone: '(313) 555-5735'
-        },
-        {
-          name: 'Maria Anders',
-          email: 'dominiqueperrier@mail.com',
-          address: '25, rue Lauriston, Paris, France',
-          phone: '(503) 555-9931'
-        },
-        {
-          name: 'Fran Wilson',
-          email: 'franwilson@mail.com',
-          address: 'C/ Araquil, 67, Madrid, Spain',
-          phone: '(204) 619-5731'
-        },
-        {
-          name: 'Martin Blank',
-          email: 'martinblank@mail.com',
-          address: 'Via Monte Bianco 34, Turin, Italy',
-          phone: '(480) 631-2097'
-        },
-        {
-          name: 'Martin Blank',
-          email: 'martinblank@mail.com',
-          address: 'Via Monte Bianco 34, Turin, Italy',
-          phone: '(480) 631-2097'
-        }
-      ]
+    openDialogEdit () {
+      this.dialogEdit = true
     },
-
-    editItem (item) {
-      this.editedIndex = this.info.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
+    openDialogAdd () {
+      this.dialogAdd = true
     },
-
-    deleteItem (item) {
-      this.editedIndex = this.info.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialogDelete = true
-    },
-
-    deleteItemConfirm () {
-      this.info.splice(this.editedIndex, 1)
-      this.closeDelete()
-    },
-
-    close () {
-      this.dialog = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
-    },
-
-    closeDelete () {
-      this.dialogDelete = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
-    },
-
-    save () {
-      if (this.editedIndex > -1) {
-        Object.assign(this.info[this.editedIndex], this.editedItem)
-      } else {
-        this.info.push(this.editedItem)
-      }
-      this.close()
+    openDialogSelectedDelete () {
+      this.dialogSelectedDelete = true
     }
   }
 }
